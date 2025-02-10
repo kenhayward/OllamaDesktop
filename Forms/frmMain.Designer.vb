567c88d9dfa9c5facde689a30aa792410b012b41
Ken Hayward <kenhayward@hotmail.com>
2025-02-10 13:58:28 +0000

    Update frmMain UI and add server loading functionality
    - Updated InitializeComponent in frmMain.Designer.vb to add new UI elements: ToolStripSeparator, ToolStripLabel, TextBox, SplitContainer, TreeView, and ToolStripDropDownButton. Increased ToolStrip size.
    - Modified ToolStrip items to include new elements and removed old ToolStripLabel.
    - Increased frmMain form's ClientSize to fit new UI elements.
    - Added LoadServers method in frmMain.vb to populate cmbModel and mnuServers with server data, called on form load and btnServers click.
    - Added ServerClick method to handle server selection from mnuServers dropdown.
    - Added GetModels method to populate cmbModel with models from the selected server.
    - Updated frmMain.resx to include new images for btnServers and mnuServers.

--- Forms/frmMain.Designer.vb
+++ Forms/frmMain.Designer.vb
@@ -23,21 +23,34 @@ Partial Class frmMain
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Dim TreeNode7 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Node2")
+        Dim TreeNode8 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Prompt Name 1", New System.Windows.Forms.TreeNode() {TreeNode7})
+        Dim TreeNode9 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("History")
         Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
         Me.btnServers = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripLabel1 = New System.Windows.Forms.ToolStripLabel()
         Me.cmbModel = New System.Windows.Forms.ToolStripComboBox()
-        Me.lblModelDetails = New System.Windows.Forms.ToolStripLabel()
+        Me.txtPrompt = New System.Windows.Forms.TextBox()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.mnuServers = New System.Windows.Forms.ToolStripDropDownButton()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.ToolStrip1.SuspendLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
         Me.SuspendLayout()
         '
         'ToolStrip1
         '
         Me.ToolStrip1.Font = New System.Drawing.Font("Segoe UI", 9.0!)
         Me.ToolStrip1.ImageScalingSize = New System.Drawing.Size(32, 32)
-        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.btnServers, Me.cmbModel, Me.lblModelDetails})
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.btnServers, Me.ToolStripSeparator1, Me.mnuServers, Me.ToolStripLabel1, Me.cmbModel})
         Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
         Me.ToolStrip1.Name = "ToolStrip1"
-        Me.ToolStrip1.Size = New System.Drawing.Size(1134, 42)
+        Me.ToolStrip1.Size = New System.Drawing.Size(1674, 42)
         Me.ToolStrip1.TabIndex = 0
         Me.ToolStrip1.Text = "ToolStrip1"
         '
@@ -50,29 +63,103 @@ Partial Class frmMain
         Me.btnServers.Size = New System.Drawing.Size(46, 36)
         Me.btnServers.Text = "Ollama Network Settings"
         '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 42)
+        '
+        'ToolStripLabel1
+        '
+        Me.ToolStripLabel1.Name = "ToolStripLabel1"
+        Me.ToolStripLabel1.Size = New System.Drawing.Size(88, 36)
+        Me.ToolStripLabel1.Text = "Model:"
+        '
         'cmbModel
         '
         Me.cmbModel.AutoSize = False
         Me.cmbModel.Name = "cmbModel"
         Me.cmbModel.Size = New System.Drawing.Size(200, 40)
         '
-        'lblModelDetails
+        'txtPrompt
         '
-        Me.lblModelDetails.Name = "lblModelDetails"
-        Me.lblModelDetails.Size = New System.Drawing.Size(299, 36)
-        Me.lblModelDetails.Text = "Parameters: 8b, Size: 23MB"
+        Me.txtPrompt.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtPrompt.Location = New System.Drawing.Point(14, 48)
+        Me.txtPrompt.Multiline = True
+        Me.txtPrompt.Name = "txtPrompt"
+        Me.txtPrompt.Size = New System.Drawing.Size(1143, 305)
+        Me.txtPrompt.TabIndex = 1
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 42)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.TreeView1)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Label1)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.txtPrompt)
+        Me.SplitContainer1.Size = New System.Drawing.Size(1674, 1147)
+        Me.SplitContainer1.SplitterDistance = 494
+        Me.SplitContainer1.SplitterWidth = 10
+        Me.SplitContainer1.TabIndex = 2
+        '
+        'TreeView1
+        '
+        Me.TreeView1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.TreeView1.Location = New System.Drawing.Point(0, 0)
+        Me.TreeView1.Name = "TreeView1"
+        TreeNode7.Name = "Node2"
+        TreeNode7.Text = "Node2"
+        TreeNode8.Name = "Node0"
+        TreeNode8.Text = "Prompt Name 1"
+        TreeNode9.Name = "Node1"
+        TreeNode9.Text = "History"
+        Me.TreeView1.Nodes.AddRange(New System.Windows.Forms.TreeNode() {TreeNode8, TreeNode9})
+        Me.TreeView1.Size = New System.Drawing.Size(492, 765)
+        Me.TreeView1.TabIndex = 0
+        '
+        'mnuServers
+        '
+        Me.mnuServers.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.mnuServers.Image = CType(resources.GetObject("mnuServers.Image"), System.Drawing.Image)
+        Me.mnuServers.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.mnuServers.Name = "mnuServers"
+        Me.mnuServers.Size = New System.Drawing.Size(113, 36)
+        Me.mnuServers.Text = "Servers"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(8, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(101, 31)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Prompt"
         '
         'frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(192.0!, 192.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
-        Me.ClientSize = New System.Drawing.Size(1134, 1031)
+        Me.ClientSize = New System.Drawing.Size(1674, 1189)
+        Me.Controls.Add(Me.SplitContainer1)
         Me.Controls.Add(Me.ToolStrip1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "frmMain"
         Me.Text = "Ollama Desktop"
         Me.ToolStrip1.ResumeLayout(False)
         Me.ToolStrip1.PerformLayout()
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.PerformLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -81,5 +168,11 @@ Partial Class frmMain
     Friend WithEvents ToolStrip1 As ToolStrip
     Friend WithEvents btnServers As ToolStripButton
     Friend WithEvents cmbModel As ToolStripComboBox
-    Friend WithEvents lblModelDetails As ToolStripLabel
+    Friend WithEvents ToolStripLabel1 As ToolStripLabel
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents txtPrompt As TextBox
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents TreeView1 As TreeView
+    Friend WithEvents mnuServers As ToolStripDropDownButton
+    Friend WithEvents Label1 As Label
 End Class
--- Forms/frmMain.vb
+++ Forms/frmMain.vb
@@ -1,7 +1,58 @@
-﻿Public Class frmMain
+﻿
+Public Class frmMain
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles Me.Load
+        LoadServers()
+        Me.Cursor = DefaultCursor
+    End Sub
+
+
     Private Sub btnServers_Click(sender As Object, e As EventArgs) Handles btnServers.Click
         Dim MyForm As New frmServers
         MyForm.ShowDialog(Me)
+        LoadServers()
+    End Sub
+
+
+    Private Sub LoadServers()
+        Me.cmbModel.Items.Clear()
+        Me.mnuServers.DropDownItems.Clear()
+        For Each Server In Utils.OllamaServers
+            Dim MyItem As New ToolStripMenuItem
+            MyItem.Text = Server.Name
+            MyItem.Tag = Server
+            If Server.isCurrent Then
+                Me.mnuServers.Text = "Server: " & Server.Name
+                Me.GetModels()
+            End If
+            AddHandler MyItem.Click, AddressOf ServerClick
+            Me.mnuServers.DropDownItems.Add(MyItem)
+        Next
+    End Sub
+
+    Private Sub ServerClick(sender As Object, e As EventArgs)
+        Dim MyItem As ToolStripMenuItem = CType(sender, ToolStripMenuItem)
+        Dim MyServer As OllamaServer = CType(MyItem.Tag, OllamaServer)
+        Utils.OllamaServers.CurrentServer = MyServer
+        Me.mnuServers.Text = "Server: " & MyServer.Name
+        Me.GetModels()
+    End Sub
+
+    Private Sub GetModels()
+        Dim MyServer As OllamaServer = Utils.OllamaServers.CurrentServer
+        If MyServer IsNot Nothing Then
+            Me.cmbModel.Items.Clear()
+            If MyServer.Models.Count = 0 Then
+                Dim MyS = New Ollama(MyServer)
+                MyS.GetModels()
+            End If
+            For Each model In MyServer.Models
+                Me.cmbModel.Items.Add(model)
+                If MyServer.DefaultModel.Equals(model) Then
+                    Me.cmbModel.SelectedItem = model
+                End If
+            Next
+        End If
     End Sub
 
 End Class
